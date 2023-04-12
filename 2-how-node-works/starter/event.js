91476432643fb6d1a39524f702faabcb3c14dff9
pcismynme <pcismyname007@gmail.com>
2023-04-12 17:21:38 +0700

    event (server response)

---
+++ 2-how-node-works/starter/event.js
@@ -0,0 +1,48 @@
+const EventEmitter = require("events");
+const http = require('http');
+
+class Sales extends EventEmitter {
+    constructor(){
+        super();
+    }
+}
+
+const myEmitter =  new Sales();
+
+
+myEmitter.on('newSale',() => {
+    console.log('There was a new Sale!');
+} );
+
+myEmitter.on('newSale', () =>  {
+    console.log("Customer name: John");
+});
+
+myEmitter.on('newSale', stock => {
+    console.log(`There are now ${stock} items left in stock`);
+})
+
+myEmitter.emit('newSale', 9);
+
+///////////////////////////////////
+
+const server = http.createServer();
+
+server.on('request', (req, res)  => {
+    console.log('requset received!');
+    console.log(req.url)
+    res.end('Requset receeived.');
+});
+
+
+server.on('request', (req, res)  => {
+    console.log('Another ☺ received!');
+});
+
+server.on('close', ()=> {
+    console.log('Server closed');
+});
+
+server.listen(8000,'127.0.0.1', ()=> {
+    console.log('Waiting for requset...');
+});
